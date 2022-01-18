b892193162eddfed59e291f4644d03b5409ead7c
liyangqiao <liyangqiao@yitu.io>
2022-01-18 13:41:23 +0800

    新增textures_4_1.html

---
+++ getting_started/textures_04/shaders/4.1.texture.fs
@@ -0,0 +1,15 @@
+#version 300 es
+precision mediump float;      // 设置浮点变量的精度
+
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+
+void main()
+{
+	FragColor = texture(texture1, TexCoord);
+}
