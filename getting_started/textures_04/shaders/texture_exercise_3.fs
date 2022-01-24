1d11700a32654608ef48552f15a8bf8af17a6734
liyangqiao <liyangqiao@yitu.io>
2022-01-24 11:43:35 +0800

    新增textures_exercise_3.html

---
+++ getting_started/textures_04/shaders/texture_exercise_3.fs
@@ -0,0 +1,17 @@
+#version 300 es
+precision mediump float;      // 设置浮点变量的精度
+
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture samplers
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+	// linearly interpolate between both textures (80% container, 20% awesomeface)
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.s, TexCoord.t)), 0.2);
+}
