6857bfe78af1648d698e999b0282af89ffc8033b
liyangqiao <liyangqiao@yitu.io>
2022-01-24 14:02:58 +0800

    新增textures_exercise_4.html

--- getting_started/textures_04/shaders/texture_exercise_4.fs
+++ getting_started/textures_04/shaders/texture_exercise_4.fs
@@ -6,6 +6,8 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
+uniform float mixNum;
+
 // texture samplers
 uniform sampler2D texture1;
 uniform sampler2D texture2;
@@ -13,5 +15,5 @@ uniform sampler2D texture2;
 void main()
 {
 	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.s, TexCoord.t)), 0.2);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.s, TexCoord.t)), mixNum);
 }
